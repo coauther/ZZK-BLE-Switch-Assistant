1a24cb405e3bda0a0611b1ccb781d258107a7b74
CHENJIE CAO <182637469+coauther@users.noreply.github.com>
2026-05-17 14:43:01 +0200

    feat: 增加ADC读取电池电量功能
    feat: Add ADC function to read battery power

--- src/main.cpp
+++ src/main.cpp
@@ -27,12 +27,12 @@ void onSwitchWrite(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uin
 
     if (command == 0x01) {
         // 收到开灯指令 (例如：舵机转动到 90 度去按压开关)
-        zzkServo.write(45);
+        zzkServo.write(20);
         delay(500);
         zzkServo.write(90);
     } else if (command == 0x00) {
         // 收到关灯指令 (例如：舵机转回 0 度复位)
-        zzkServo.write(135);
+        zzkServo.write(120);
         delay(500);
         zzkServo.write(90);
     }
@@ -49,6 +49,10 @@ void setup() {
     // 1. 物理层省电：彻底切断 USB 和开启高效 DCDC
     NRF_POWER->DCDCEN = 1; // 开启 DCDC，大幅降低运行功耗 [cite: 177]
     NRF_USBD->ENABLE = 0;  // 彻底关掉 USB 外设，释放 HFCLK 时钟锁
+
+    // 2. ADC 分辨率配置 (关键新增)
+    // 将 ADC 分辨率设置为 10 位 (0-1023)，与你之前测出的 raw 值匹配 [cite: 246]
+    analogReadResolution(10);
 
     // 关闭 LED
     pinMode(LED_BUILTIN, OUTPUT);
@@ -70,9 +74,9 @@ void setup() {
     switchChar.begin();
 
     batteryChar.setProperties(CHR_PROPS_READ | CHR_PROPS_NOTIFY);
-    batteryChar.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+    batteryChar.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+    batteryChar.setFixedLen(1);
     batteryChar.begin();
-    batteryChar.write8(100);
 
     // 2. 优化广播策略：解决名字截断问题
     // 主广播包：只放 Flags 和 名字
@@ -89,20 +93,28 @@ void setup() {
 }
 
  void loop() {
-//     static uint32_t last_check = 0;
-//
-//     // 每 10 秒尝试读取并上报电量
-//     if (millis() - last_check > 10000) {
-//         last_check = millis();
-//
-//         // 读取 PIN_VBAT 处的真实电压（ADC 读取逻辑）
-//         int raw = analogRead(PIN_VBAT);
-//         int percentage = map(raw, 700, 890, 0, 100);
-//         uint8_t battery_level = (uint8_t)constrain(percentage, 0, 100);
-//
-//         // 主动将电量推送（Notify）到网页端 [cite: 622]
-//         batteryChar.notify8(battery_level);
-//     }
-//
-//     delay(200); // 这里的延时会让 FreeRTOS 调度 CPU 进入浅睡 [cite: 157, 180]
+     static uint32_t last_check = 0;
+
+     // 每 10 秒尝试读取并上报电量
+     if (millis() - last_check > 10000) {
+         last_check = millis();
+
+         // 读取 PIN_VBAT 处的真实电压（ADC 读取逻辑）
+         int raw = analogRead(PIN_VBAT);
+
+         // 将 10 位 ADC 的裸数据映射为 0-100 的百分比
+         // 提示：700 到 890 这个区间是基于标准 3.7V 锂电池放电曲线的经验值
+         int percentage = map(raw, 700, 890, 0, 100);
+
+         // 限制数值在 0-100 之间，防止过充或过放时数据溢出 [cite: 57]
+         uint8_t battery_level = (uint8_t)constrain(percentage, 0, 100);
+
+         // 主动将电量推送（Notify）给已连接的网关或网页 [cite: 57]
+         if (Bluefruit.connected()) {
+             batteryChar.notify8(battery_level);
+         }
+     }
+
+    // 呼叫 RTOS 调度器让出 CPU，进入休眠 [cite: 58]
+    delay(100);
  }
